008b9297db3b088f042be39a13c555fddc5c9923
renetan <nikolcho.r@gmail.com>
2015-06-26 18:34:09 +0300

    Create RandomG.cpp

---
+++ RandomG.cpp
@@ -0,0 +1,35 @@
+#include "Random.h"
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+Point2D RandomG::getRanodmCordinates()
+{
+	Point2D position;
+	srand(time(NULL));             // srand() gives the random fucntion a new starting point time(NULL)garanties getting a new starting point every time
+	double x1 = rand() % 100 + 0;  //generates random numbers from 0 to 100
+	double y1 = rand() % 100 + 0;
+	position = Point2D(x1, y1);
+	return position;
+}
+
+void RandomGenerator::getRandomDamage(Entity& smth)
+{
+	srand(time(NULL));                   
+	int code = rand() % 3 + 1;          //random number between 1-3
+	switch (code)
+	{
+	case 1:
+		smth.setEnergy(smth.getEnergy() - 3);
+		break;
+	case 2:
+		smth.setStrenght(smth.getStrenght() - 3);
+		break;
+	case 3:
+		smth.setEnergy(smth.getEnergy() - 1);
+		smth.setStrenght(smth.getEnergy() - 1);
+		break;
+	}
+}
