ee6331122ea2c1181df0502b57f3739bd27a9117
renetan <nikolcho.r@gmail.com>
2015-06-26 20:01:34 +0300

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -9,9 +9,9 @@ public:
   Animal();
   Animal(string name,double energy,double power,double size,double weight, Point2D point,State state); 
 
-	virtual void Eat();
-	virtual void Sleep();
-	virtual void SearchingForFood();
+	void Eat();
+	void Sleep();
+	void SearchingForFood();
 };
 
 #endif
