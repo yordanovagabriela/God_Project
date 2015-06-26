6e2eec35736759536df13ff95f1607678ef9ff8b
Gabriela Yordanova <gabriela.georgieva.yordanova@gmail.com>
2015-06-26 22:31:33 +0300

    Update Animal.cpp

--- Animal.cpp
+++ Animal.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 Animal::Animal() 
 { 
-  this->name = " ";
-  this->energy = 0;
-  this->power = 0;
-  this->size = 0;
-  this->weight = 0;
-  this->point = new Point2D();
-  this->state = State.Unknown;
+  name = " ";
+  energy = 0;
+  power = 0;
+  size = 0;
+  weight = 0;
+  point = new Point2D();
+  state = State.Unknown;
 }
 
-Animal::Animal(string name, double energy, double power, double size, double weight, Point2D point, State state) : Entity(name, energy, power, size, weight, point, state)
+Animal::Animal(string name, double energy, double power, double size, double weight, Point2D *point, State state) : Entity(name, energy, power, size, weight, point, state)
 {      
 }                   
 
